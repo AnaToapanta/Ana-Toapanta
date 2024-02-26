14690e10a0b84c6c0af8e94ccbf7c880850873b2
Jordy Parion <jj.pariona@uea.edu.ec>
2024-02-25 20:36:04 -0500

    TRABAJO SEMANA 11

---
+++ 2 PROGRAMA.py
@@ -0,0 +1,11 @@
+matriz = [
+    [11, 40, 12],
+    [32, 80, 14],
+    [25, 36, 89]
+]
+
+for fila in matriz:
+    #print(fila)
+    #fila.sort(reverse=True)
+    #print(fila)
+    print(sorted(fila))
